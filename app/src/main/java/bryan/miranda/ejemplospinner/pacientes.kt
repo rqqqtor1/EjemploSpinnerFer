bf4afbace5d8a51b2a46c3b834df396b8947f992
- <->
2024-06-13 10:31:49 -0600

    spinner funcionando

--- app/src/main/java/bryan/miranda/ejemplospinner/pacientes.kt
+++ app/src/main/java/bryan/miranda/ejemplospinner/pacientes.kt
@@ -5,6 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.ClaseConexion
+import modelo.dataClassDoctores
 
 class pacientes : Fragment() {
 
@@ -19,6 +27,44 @@ class pacientes : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_pacientes, container, false)
+        val root = inflater.inflate(R.layout.fragment_pacientes, container, false)
+        val spDoctores = root.findViewById<Spinner>(R.id.spDoctores)
+
+        fun obtenerDoctores(): List<dataClassDoctores> {
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            val statement = objConexion?.createStatement()
+            val resulSet = statement?.executeQuery("select * from tbDoctores")!!
+
+            val ListaDoctores = mutableListOf<dataClassDoctores>()
+
+            while (resulSet.next()){
+                val uuid = resulSet.getString("DoctorUUID")
+                val nombre = resulSet.getString("nombreDoctor")
+                val especialidad = resulSet.getString("Especialidad")
+                val telefono = resulSet.getString("Telefono")
+                val unDoctorCompleto = dataClassDoctores(uuid, nombre, especialidad, telefono)
+                ListaDoctores.add(unDoctorCompleto)
+            }
+            return ListaDoctores
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val ListadeDoctores = obtenerDoctores()
+            val nombreDoctores = ListadeDoctores.map { it.nombreDoctor }
+
+            //Crear adaptador
+withContext(Dispatchers.Main) {
+    val miAdaptador = ArrayAdapter(
+        requireContext(),
+        android.R.layout.simple_spinner_dropdown_item,
+        nombreDoctores
+    )
+    spDoctores.adapter = miAdaptador
+}
+        }
+
+
+        return root
     }
 }
--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -6,9 +6,9 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion(): Connection? {
         try {
-            val url = "jdbc:oracle:thin:@10.10.0.53:1521:xe"
-            val usuario = "system"
-            val contrasena = "desarrollo"
+            val url = "jdbc:oracle:thin:@10.10.0.144:1521:xe"
+            val usuario = "FER_DEVELOPER"
+            val contrasena = "123456"
 
             val connection = DriverManager.getConnection(url, usuario, contrasena)
             return connection
